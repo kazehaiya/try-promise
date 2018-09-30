8098a79df56d22f0b9078d3e995a072cb5d6b271
caiyin <caiyin@zuoyebang.com>
2018-09-30 16:57:58 +0800

    feat: 完成 then 的初版处理，待测试

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-1. 实现回调函数
+## 术语
+- `promise` 是一个包含了兼容promise规范then方法的对象或函数
+- `thenable` 是一个包含了then方法的对象或函数
+- `value` 是任何Javascript值。 (包括 undefined, thenable, promise等)
+- `exception` 是由throw表达式抛出来的值
+- `reason` 是一个用于描述Promise被拒绝原因的值
+
+## 要求
+#### Promise 状态
+1. 一个Promise必须处在其中之一的状态：pending, fulfilled 或 rejected.<br>
+pending 可转状态为 fulfilled 和 rejected，其余状态不可更改（代码中用的是 resolved ）
+
+2. **then 方法**
+
+
+> 内容来自：[Promise/A+ 规范](https://segmentfault.com/a/1190000002452115)
+
+## 学习心得
+#### Promise 最初的流程
+1. 先处理 new Promise() 内部的同步任务，遇见异步会压入宏任务队列，然后执行后面的内容
+> 注：此时如果 throw Error('reason') 就不能走 then 路线，而是 catch 路线
+2. 看是否有 then 链，如果有则继续走 then 链的内容，由于 resolve 和 reject 都为异步队列，因此此时的状态都为 pending ，由 规则Promise[3.3]，构建一个新的 promise 函数将值压入待处理的队列中
--- try_promise.js
+++ try_promise.js
@@ -1,67 +1,182 @@
 const PENDING = 'pending';
 const RESOLVED = 'resolved';
 const REJECTED = 'rejected';
+const REASON = 'promise resolver is not a function at new promise!';
 
 /**
- * 仿写实现 Promise
+ * 仿写 promise
  *
  * @class promise
  */
 class promise {
   constructor(resolver) {
     // 传入的非函数则报错
-    if (Object.prototype.toString.call(resolver) !== '[object Function]') {
-      throw Error('promise resolver is not a function at new promise!');
+    if (!this._isFunction(resolver)) {
+      throw Error(REASON);
     }
     // 初始化状态
     this.currentStatus = PENDING;
     // 两种状态的数组（仅当有 then 时存入待处理的内容）
-    this.resolverArr = [];
+    this.resolvedArr = [];
     this.rejectedArr = [];
     // 异步的结果
     this.result = undefined;
-    // 成功时的回调
-    this.resolved = result => {
-      // then 链传入的为 promise 的情况
-      if (result instanceof promise) {
-        return result(this.resolved, this.rejected);
+
+    // 选择 resolved 或者成功时的回调
+    this.resolved = value => {
+      // resolved 中传入的参数为 promise 的情况
+      if (value instanceof promise) {
+        return value(this.resolved, this.rejected);
       }
-      // 其他情况
+      // 其他情况放入下一宏任务队列，依次执行
       setTimeout(() => {
         if (this.currentStatus === PENDING) {
           this.currentStatus = RESOLVED;
-          this.result = result;
-          this.resolverArr.forEach(callback => callback(), 0);
+          this.result = value;
+          // 处理 then 链压入的队列
+          this.resolvedArr.forEach(callback => callback());
         }
       });
     };
-    // 失败时的回调
+    // 选择 rejected 或者出错时的回调
     this.rejected = reason => {
+      // 放入下一宏任务队列，依次执行
       setTimeout(() => {
         if (this.currentStatus === PENDING) {
           this.currentStatus = REJECTED;
           this.result = reason;
-          this.rejectedArr.forEach(callback => callback(), 0);
+          // 处理 then 链压入的队列
+          this.rejectedArr.forEach(callback => callback());
         }
       });
     };
-    // 执行回调（捕获传入函数的 throw Error 报错）
+
+    // 执行回调函数（捕获传入函数的 throw Error 报错）
     try {
       resolver(this.resolved, this.rejected);
-    } catch (e) {
-      this.rejected(e);
+    } catch (reason) {
+      this.rejected(reason);
     }
   }
 
-  // then 链，处理各种状态
-  then() {}
+  /**
+   * then 链
+   *
+   * @param {Function} onResolved 可选参数，非函数可忽略（规范2.2）
+   * @param {Function} onRejected 可选参数，非函数可忽略（规范2.2）
+   * @memberof promise
+   */
+  then(onResolved, onRejected) {
+    onResolved = this._isFunction(onResolved) ? onResolved : value => value;
+    onRejected = this._isFunction(onRejected) ? onRejected : () => { throw Error(REASON); };
+    // 判断当前的状态，每次都返回一个新的 promise 实例（规范3.3）
+    let statusNow = this.currentStatus;
+    let promiseNext = null;
+    // pending 状态
+    if (statusNow === PENDING) {
+      promiseNext = new promise((resolved, rejected) => {
+        // 处理 resolved 部分函数
+        this._setResolvedList(onResolved, resolved, rejected);
+        // 处理 reject 部分函数
+        this._setRejectedList(onRejected, resolved, rejected);
+      })
+    }
+    // resolved 状态
+    if (statusNow === RESOLVED) {
+      promiseNext = new promise((resolved, rejected) => {
+        setTimeout(() => {
+          this._setResolvedList(onResolved, resolved, rejected);
+        })
+      });
+    }
+    // rejected 状态
+    if (statusNow === REJECTED) {
+      promiseNext = new promise((resolved, rejected) => {
+        setTimeout(() => {
+          this._setRejectedList(onRejected, resolved, rejected);
+        })
+      });
+    }
+    // 返回新的 promise
+    return promiseNext;
+  }
+
+  /**
+   * 异常捕捉
+   *
+   * @memberof promise
+   */
+  catch() {}
+
+  /**
+   * 为 resolvedArr 列表添加回调项
+   *
+   * @param {*} onResolved 回调函数
+   * @param {*} resolved
+   * @param {*} rejected
+   * @memberof promise
+   */
+  _setResolvedList(onResolved, resolved, rejected) {
+    this.resolvedArr.push(() => {
+      // 考虑到传入的函数为 () => { throw Error('reason') }
+      try {
+        // 获取当前 then 函数的返回值（入参为前者的结果）
+        const callback = onResolved(this.result);
+        // 单个 then 链
+        resolved(callback);
+      } catch(reason) {
+        rejected(reason);
+      }
+    });
+  }
+
+  /**
+   * 为 resolvedArr 列表添加回调项
+   *
+   * @param {*} onResolved
+   * @param {*} resolved
+   * @param {*} rejected
+   * @memberof promise
+   */
+  _setRejectedList(onRejected, resolved, rejected) {
+    this.rejectedArr.push(() => {
+      // 考虑到传入的函数为 () => { throw Error('reason') }
+      try {
+        // 获取当前 then 函数的返回值（入参为前者的结果）
+        let callback = onRejected(this.result);
+        // 单个 then 链
+        resolved(callback);
+      } catch(reason) {
+        rejected(reason);
+      }
+    });
+  }
+
+  /**
+   * 判断是否是函数(内部方法)
+   *
+   * @param {Function} target
+   * @memberof promise
+   */
+  _isFunction(target) {
+    return Object.prototype.toString.call(target) === '[object Function]';
+  }
 }
 
-let test = new promise(() => {
-  new promise(() => {
-    console.log('test');
-  });
-  // throw Error('error');
-});
+new promise((resolved, rejected) => {
+  resolved(1);
+}).then(res => {
+  console.log(res);
+}).then(console.log('test'));
 
-console.log(test);
+// let test = new promise(() => {
+//   new promise(() => {
+//     console.log('test');
+//   });
+// });
+// console.log(test);
+
+
+// new promise(() => {
+//   throw Error('error');
+// });
